23f9fc666fdde246f480815f7978f8847574372d
ingridmidia <ingridmidia@gmail.com>
2023-09-27 19:51:58 -0500

    added event listener on buttons and saved to local storage

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+
+  var appointments = {};// object that will store appointment description
+
+  $(".btn").on("click", function () {
+    var description = $(this).siblings(".description").val();// gets the value of button's sibling description
+    var timeBlock = $(this).parent().attr("id");// gets the id of button's parent time block
+    if (localStorage.getItem("appointments") === null) {
+      appointments = {};// if local storage is empty, replace for an empty object
+    } else {
+      appointments = JSON.parse(localStorage.getItem("appointments"));// gets local storage string and turn into an object
+    }
+    appointments[timeBlock] = description;// creates an object key using the timeBlock id(button's parent) and the value is description(value of button's sibling)
+    localStorage.setItem("appointments", JSON.stringify(appointments));// set the object appointments as a string on localStorage
+  });
 });
 
-$("#currentDay").text(dayjs().format("dddd, MMMM D"));// Day of Month with ordinal?
