a6eebdf7672a045d514152db9a12dcdf6d6656f1
ingridmidia <ingridmidia@gmail.com>
2023-09-27 21:46:45 -0500

    refactored appointments

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 $(function () {
   $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 
-  var appointments = {};// object that will store appointment description
+  var appointments = JSON.parse(localStorage.getItem("appointments")) || [];// array that will store new appointments
 
   $(".btn").on("click", function () {
     var description = $(this).siblings(".description").val();// gets the value of button's sibling description
     var timeBlock = $(this).parent().attr("id");// gets the id of button's parent time block
-    if (localStorage.getItem("appointments") === null) {
-      appointments = {};// if local storage is empty, replace for an empty object
-    } else {
-      appointments = JSON.parse(localStorage.getItem("appointments"));// gets local storage string and turn into an object
+
+    var newAppointment = { // object that will appointment time and user description
+      hour: timeBlock,
+      description: description,
     }
-    appointments[timeBlock] = description;// creates an object key using the timeBlock id(button's parent) and the value is description(value of button's sibling)
+
+    var appointmentAlreadyExists = false;
+    //checks if an appointment already exists for the hour
+    for (var i = 0; i < appointments.length; i++) {
+      if (appointments[i].hour === newAppointment.hour) {
+        appointments[i].description = newAppointment.description;// replaces description 
+        appointmentAlreadyExists = true;
+      }
+    }
+    // if appointment does not exist for the hour, add new appointment
+    if (!appointmentAlreadyExists) {
+      appointments.push(newAppointment);
+    }
+
     localStorage.setItem("appointments", JSON.stringify(appointments));// set the object appointments as a string on localStorage
   });
 });
